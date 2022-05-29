6617581cdbdf940a63cbf27f82d3b4aaaac14513
gonzalohg <gonzalo.hg19@gmail.com>
2022-05-28 19:38:13 -0500

    Se agrega servicio para consulta egresado flexible

--- src/main/java/com/usuario/service/repositorio/UsuarioRepository.java
+++ src/main/java/com/usuario/service/repositorio/UsuarioRepository.java
@@ -1,5 +1,9 @@
 package com.usuario.service.repositorio;
 
-public interface UsuarioRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.usuario.service.entidades.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
 
 }
